4a52fdab34fab1482a1ac55c202e4726ea193f11
José Antonio Parejo Maestre <japarejo@gmail.com>
2024-09-10 16:00:00 +0200

    Fixed type of primary key in USer and Authorities repositories

--- src/main/java/es/us/dp1/lx_xy_24_25/your_game_name/user/UserRepository.java
+++ src/main/java/es/us/dp1/lx_xy_24_25/your_game_name/user/UserRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 
-public interface UserRepository extends  CrudRepository<User, String>{			
+public interface UserRepository extends  CrudRepository<User, Integer>{			
 
 
 	Optional<User> findByUsername(String username);
