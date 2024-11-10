aad8a5db1470290def00b26794a10d6482d632fd
japarejo <japarejo@gmail.com>
2024-11-10 19:43:42 +0100

    Fixed bug in the repository query!

--- src/main/java/es/us/dp1/lx_xy_24_25/your_game_name/user/User.java
+++ src/main/java/es/us/dp1/lx_xy_24_25/your_game_name/user/User.java
@@ -8,13 +8,14 @@ import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
 
 import es.us.dp1.lx_xy_24_25.your_game_name.model.BaseEntity;
-
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 @Entity
+@EqualsAndHashCode(of = "id")
 @Table(name = "appusers")
 public class User extends BaseEntity {
 
