051ee14615d3e8344c57a3cdb22d7d0ad261f9ad
José Antonio Parejo Maestre <japarejo@gmail.com>
2025-11-06 13:56:29 +0100

    Removed unused import!

--- src/main/java/es/us/dp1/lx_xy_24_25/your_game_name/match/Match.java
+++ src/main/java/es/us/dp1/lx_xy_24_25/your_game_name/match/Match.java
@@ -11,7 +11,6 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Transient;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
-import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
