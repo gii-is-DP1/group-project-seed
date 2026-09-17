c9f3021c192281989ef260d960312c8c31ab953c
Adartse <bediliaestrada@gmail.com>
2026-09-17 17:26:30 +0200

    chore: update file references to 2026–2027

---
+++ src/main/java/es/us/dp1/lx_y_26_27/my_game/GameApplication.java
@@ -0,0 +1,13 @@
+package es.us.dp1.lx_y_26_27.my_game;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication()
+public class GameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameApplication.class, args);
+	}
+
+}
---
+++ src/main/java/es/us/dp1/lx_y_26_27/my_game/model/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * The classes in this package represent PetClinic's business layer.
+ */
+package es.us.dp1.lx_y_26_27.my_game.model;
